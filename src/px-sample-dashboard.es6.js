32a4d1bb8842560a33e7672b8d7bb40ab2fab5f9
Randy Askin <randall.askin@ge.com>
2017-10-24 15:48:32 -0700

    map updates

--- src/px-sample-dashboard.es6.js
+++ src/px-sample-dashboard.es6.js
@@ -11,7 +11,28 @@
         value: function() {
           return {};
         }
+      },
+      _mapData: {
+        type: Object,
+        value: function() {
+          return {};
+        }
       }
+    },
+    attached() {
+      this.set('_mapData', Object.assign({}, this.mapData));
+      var markers = this.$.markers;
+      markers.set('data', this._mapData);
+      markers.update();
+    },
+    updateMap(evt) {
+      var percent = 800 * parseInt(evt.detail.value) / 100;
+      var arr = this.mapData.features.slice(0, percent);
+      this.set('_mapData.type', 'FeatureCollection');
+      this.set('_mapData.features', arr);
+      var markers = this.$.markers;
+      markers.set('data', this._mapData);
+      markers.update();
     }
   });
 })();
