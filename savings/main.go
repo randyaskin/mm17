73e5498b9cfdab37344684d39ba395415c15a30c
Grant Griffiths <grant.griffiths@ge.com>
2017-10-25 13:22:51 -0700

    add subsidies and total

--- savings/main.go
+++ savings/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math"
 	"net/http"
 	"os"
 	"strconv"
@@ -17,6 +18,9 @@ type savingsReport struct {
 	PredictedConsumption  float64     `json:"PredictedConsumptionKWH"`
 	SolarOffset           float64     `json:"SolarOffsetKWH"`
 	HistoricalConsumption []DataPoint `json:"HistoricalConsumptionKWH"`
+	Subsidies             []float64   `json:"Subsidies"`
+	TotalSubsidy          float64     `json:"TotalSubsidy"`
+	AvgSubsidy            float64     `json:"AvgSubsidy"`
 }
 
 var sdEnergyHistorical = []float64{14395.6602, 13768.9679, 14501.41184, 14421.33019, 14733.81518, 14898.06997, 15359.51847, 15979.35501, 15489.09043, 13984.35539, 17475.61581, 16741.03086, 17120.1648, 17772.47179, 18706.83082, 18714.43401, 19664.75597, 19638.47435, 19994.82863, 19515.30326, 18978.25227, 19022.59425, 19562.03057, 19425.7796, 19903.80127, 19781.17809}
@@ -35,6 +39,26 @@ const (
 	resEnergyPerInstall = 6800
 	comEnergyPerInstall = 81600
 	indEnergyPerInstall = 108800
+
+	bracket1A     = 1200
+	bracket1B     = 1500000000
+	bracket1Scale = 10000
+
+	bracket2A     = 16000
+	bracket2B     = 1500000000
+	bracket2Scale = 9000
+
+	bracket3A     = 26000
+	bracket3B     = 1500000000
+	bracket3Scale = 8000
+
+	bracket4A     = 37000
+	bracket4B     = 1500000000
+	bracket4Scale = 7000
+
+	bracket5A     = 31000
+	bracket5B     = 1500000000
+	bracket5Scale = 5000
 )
 
 var historicalConsumption []DataPoint
@@ -61,7 +85,6 @@ func main() {
 	}
 
 	http.HandleFunc("/v1/savings", savingsHandler)
-	http.HandleFunc("/v1/subsidy", subsidyHandler)
 	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
@@ -98,6 +121,34 @@ func savingsHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	var bracketCounts []float64
+	var subsidies []float64
+	for i := 1; i <= 9; i++ {
+		var subsidy float64
+		bracketCount, err := strconv.Atoi(r.URL.Query().Get(fmt.Sprintf("p%v", i)))
+		if err != nil {
+			w.WriteHeader(500)
+			w.Write([]byte(fmt.Sprintf("Error occurred: %v", err)))
+			return
+		}
+		switch i {
+		case 5:
+			subsidy = bellCurve(bracket1Scale, bracket1A, bracket1B, float64(bracketCount))
+		case 6:
+			subsidy = bellCurve(bracket2Scale, bracket2A, bracket2B, float64(bracketCount))
+		case 7:
+			subsidy = bellCurve(bracket3Scale, bracket3A, bracket3B, float64(bracketCount))
+		case 8:
+			subsidy = bellCurve(bracket4Scale, bracket4A, bracket4B, float64(bracketCount))
+		case 9:
+			subsidy = bellCurve(bracket5Scale, bracket5A, bracket5B, float64(bracketCount))
+		default:
+			subsidy = 0
+		}
+		subsidies = append(subsidies, subsidy)
+		bracketCounts = append(bracketCounts, float64(bracketCount))
+	}
+
 	solarGeneration := getSolarGeneration(comCount, resCount, indCount)
 	predictedConsumption, err := getPredictedConsumption(targetYear)
 	if err != nil {
@@ -105,60 +156,25 @@ func savingsHandler(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(fmt.Sprintf("Error occurred: %v", err)))
 		return
 	}
+
+	var totalSubsidy float64
+	for i, s := range subsidies {
+		totalSubsidy = totalSubsidy + s*bracketCounts[i]
+	}
+
+	var totalPeople float64
+	for _, bc := range bracketCounts {
+		totalPeople = totalPeople + bc
+	}
 	sr := savingsReport{
 		InitialCost:           getInitialCost(comCount, resCount, indCount),
 		SolarEnergyGeneration: solarGeneration,
 		PredictedConsumption:  predictedConsumption,
 		SolarOffset:           predictedConsumption - float64(solarGeneration),
+		Subsidies:             subsidies,
+		TotalSubsidy:          totalSubsidy,
+		AvgSubsidy:            totalSubsidy / totalPeople,
 		// HistoricalConsumption: historicalConsumption,
-	}
-
-	respBytes, err := json.Marshal(sr)
-	if err != nil {
-		w.WriteHeader(500)
-		w.Write([]byte(fmt.Sprintf("Error occurred: %v", err)))
-		return
-	}
-
-	w.Write(respBytes)
-}
-
-func subsidyHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	cnYear, err := strconv.Atoi(r.URL.Query().Get("cn"))
-	if err != nil {
-		w.WriteHeader(500)
-		w.Write([]byte(fmt.Sprintf("Error occurred: %v", err)))
-		return
-	}
-	comCount, err := strconv.Atoi(r.URL.Query().Get("com"))
-	if err != nil {
-		w.WriteHeader(500)
-		w.Write([]byte(fmt.Sprintf("Error occurred: %v", err)))
-		return
-	}
-	resCount, err := strconv.Atoi(r.URL.Query().Get("res"))
-	if err != nil {
-		w.WriteHeader(500)
-		w.Write([]byte(fmt.Sprintf("Error occurred: %v", err)))
-		return
-	}
-	indCount, err := strconv.Atoi(r.URL.Query().Get("ind"))
-	if err != nil {
-		w.WriteHeader(500)
-		w.Write([]byte(fmt.Sprintf("Error occurred: %v", err)))
-		return
-	}
-
-	totalConsumption := 2000000000
-	solarGeneration := getSolarGeneration(comCount, resCount, indCount)
-	sr := savingsReport{
-		InitialCost:           getInitialCost(comCount, resCount, indCount),
-		TotalCityConsumption:  totalConsumption,
-		SolarEnergyGeneration: solarGeneration,
-		EnergyOffset:          totalConsumption - solarGeneration,
 	}
 
 	respBytes, err := json.Marshal(sr)
@@ -235,3 +251,18 @@ func getPredictedConsumption(targetYear int) (float64, error) {
 
 // 	return 0
 // }
+
+func bellCurve(scale, a, b, x float64) float64 {
+	//	return 5000e^(-(x-5000)^2/(3000))
+	// fmt.Println("ANS", math.Pow(2.71828182, (x-a)*(x-a)*-1))
+	y := ((x - a) * (x - a) * -1) / b
+	val := scale * math.Pow(math.E, y)
+	fmt.Println("scale", scale)
+	fmt.Println("a", a)
+	fmt.Println("b", b)
+	fmt.Println("x", x)
+	fmt.Println("y", y)
+	fmt.Println("val", val)
+	fmt.Println()
+	return val
+}
