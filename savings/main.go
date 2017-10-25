55941e962576e17c326feb84929e591186d19e8a
Grant Griffiths <grant.griffiths@ge.com>
2017-10-24 19:55:12 -0700

    add savings prediction

--- savings/main.go
+++ savings/main.go
@@ -7,13 +7,23 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+
+	"github.com/sajari/regression"
 )
 
 type savingsReport struct {
-	InitialCost           int `json:"InitialCost"`
-	TotalCityConsumption  int `json:"TotalCityConsumption"`
-	SolarEnergyGeneration int `json:"SolarEnergyGeneration"`
-	EnergyOffset          int `json:"EnergyOffset"`
+	InitialCost           int         `json:"InitialCost"`
+	SolarEnergyGeneration int         `json:"SolarEnergyGenerationKWH"`
+	PredictedConsumption  float64     `json:"PredictedConsumptionKWH"`
+	SolarOffset           float64     `json:"SolarOffsetKWH"`
+	HistoricalConsumption []DataPoint `json:"HistoricalConsumptionKWH"`
+}
+
+var sdEnergyHistorical = []float64{14395.6602, 13768.9679, 14501.41184, 14421.33019, 14733.81518, 14898.06997, 15359.51847, 15979.35501, 15489.09043, 13984.35539, 17475.61581, 16741.03086, 17120.1648, 17772.47179, 18706.83082, 18714.43401, 19664.75597, 19638.47435, 19994.82863, 19515.30326, 18978.25227, 19022.59425, 19562.03057, 19425.7796, 19903.80127, 19781.17809}
+
+type DataPoint struct {
+	Year   int     `json:"Year"`
+	Energy float64 `json:"Energy"`
 }
 
 const (
@@ -26,7 +36,19 @@ const (
 	indEnergyPerInstall = 20400
 )
 
+var historicalConsumption []DataPoint
+var currentYear = 2015
+
 func main() {
+	startYear := 1990
+	for i, val := range sdEnergyHistorical {
+		historicalConsumption = append(historicalConsumption, DataPoint{
+			Year:   startYear + i,
+			Energy: val,
+		})
+
+	}
+
 	http.HandleFunc("/v1/savings", savingsHandler)
 	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil {
@@ -56,15 +78,29 @@ func savingsHandler(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(fmt.Sprintf("Error occurred: %v", err)))
 		return
 	}
+	targetYear, err := strconv.Atoi(r.URL.Query().Get("targetYear"))
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf("Error occurred: %v", err)))
+		return
+	}
 
-	totalConsumption := 2000000000
 	solarGeneration := getSolarGeneration(comCount, resCount, indCount)
+	predictedConsumption, err := getPredictedConsumption(targetYear)
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf("Error occurred: %v", err)))
+		return
+	}
 	sr := savingsReport{
 		InitialCost:           getInitialCost(comCount, resCount, indCount),
-		TotalCityConsumption:  totalConsumption,
 		SolarEnergyGeneration: solarGeneration,
-		EnergyOffset:          totalConsumption - solarGeneration,
+		PredictedConsumption:  predictedConsumption,
+		SolarOffset:           predictedConsumption - float64(solarGeneration),
+		// HistoricalConsumption: historicalConsumption,
 	}
+
+	fmt.Println("sr", sr)
 
 	respBytes, err := json.Marshal(sr)
 	if err != nil {
@@ -83,3 +119,52 @@ func getInitialCost(resCount, comCount, indCount int) int {
 func getSolarGeneration(resCount, comCount, indCount int) int {
 	return (resCount * resEnergyPerInstall) + (comCount * comEnergyPerInstall) + (indCount * indEnergyPerInstall)
 }
+
+func getPredictedConsumption(targetYear int) (float64, error) {
+	years := targetYear - currentYear
+	r := new(regression.Regression)
+	r.SetObserved("City of San Diego Energy usage")
+	r.SetVar(0, "Year")
+	r.SetVar(1, "Energy consumption in GWh")
+	for i, year := range sdEnergyHistorical {
+		r.Train(regression.DataPoint(year, []float64{float64(i + 1990)}))
+	}
+	r.Run()
+
+	var forecastedYears []DataPoint
+	for i := 0; i < years; i++ {
+		year := currentYear + i
+		fc, err := r.Predict([]float64{float64(year)})
+		if err != nil {
+			return 0, err
+		}
+		forecastedYears = append(forecastedYears, DataPoint{
+			Year:   year,
+			Energy: fc,
+		})
+	}
+
+	fc, err := r.Predict([]float64{float64(targetYear)})
+	if err != nil {
+		return 0, err
+	}
+
+	// convert to kWh
+	fc = fc * 1000000
+
+	return fc, nil
+}
+
+// old code to get total consumption
+// var forecastedYears []DataPoint
+// for i := 0; i < years; i++ {
+// 	year := currentYear + i
+// 	fc, err := r.Predict([]float64{float64(year)})
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	forecastedYears = append(forecastedYears, DataPoint{
+// 		Year:   year,
+// 		Energy: fc,
+// 	})
+// }
