21af3eaa1d2093b901fffa644daa4ea943bb9a65
Williams, Michael <michael.williams3@ge.com>
2017-10-25 10:44:23 -0700

    added economic slider adjustments

--- src/px-sample-dashboard.es6.js
+++ src/px-sample-dashboard.es6.js
@@ -1391,6 +1391,78 @@
         type: Number,
         value: 0,
         observer: 'sliderChanged'
+      },
+      _inc1: {
+        type: Number,
+        value: 0
+      },
+      _inc2: {
+        type: Number,
+        value: 0
+      },
+      _inc3: {
+        type: Number,
+        value: 0
+      },
+      _inc4: {
+        type: Number,
+        value: 0
+      },
+      _inc5: {
+        type: Number,
+        value: 0
+      },
+      _inc6: {
+        type: Number,
+        value: 0
+      },
+      _inc7: {
+        type: Number,
+        value: 0
+      },
+      _inc8: {
+        type: Number,
+        value: 0
+      },
+      _inc9: {
+        type: Number,
+        value: 0
+      },
+      _inc1Perc: {
+        type: Number,
+        value: 0
+      },
+      _inc2Perc: {
+        type: Number,
+        value: 0
+      },
+      _inc3Perc: {
+        type: Number,
+        value: 0
+      },
+      _inc4Perc: {
+        type: Number,
+        value: 0
+      },
+      _inc5Perc: {
+        type: Number,
+        value: 0
+      },
+      _inc6Perc: {
+        type: Number,
+        value: 0
+      },
+      _inc7Perc: {
+        type: Number,
+        value: 0
+      },
+      _inc8Perc: {
+        type: Number,
+        value: 0
+      },
+      _inc9Perc: {
+        type: Number,
+        value: 0
       }
     },
     attached() {
@@ -1416,7 +1488,6 @@
       this._carbonOffset = (this._savingsResponse.SolarOffsetKWH / 1000000).toFixed(0);
     },
     sliderChanged(e) {
-      console.log("world");
       var base = "https://savings-app.run.aws-usw02-pr.ice.predix.io";
 
       var res = (this._residentialSliderPercentage / 100) * 500000;
@@ -1425,8 +1496,66 @@
       var cn = this._carbonNuetralDate;
 
       this.$.savingsCalc.url = base + "/v1/savings?com=" + com + "&res=" + res + "&ind=" + ind + "&targetYear=" + cn;
-      console.log(this.$.savingsCalc.url)
       this.$.savingsCalc.generateRequest();
+
+      this._adjustEconomicNumbers(res, this._residentialSliderPercentage);
+    },
+    _adjustEconomicNumbers(res, perc) {
+      if(perc < 20) {
+        if((res * .65) >= 79409) {
+          this._inc9 = 79409;   
+          this._inc9Perc = (100).toFixed(2);          
+        } else {
+          this._inc9 = (res * .75).toFixed(0);
+          this._inc9Perc = ( ( ( res * .75) /  79409 ) * 100).toFixed(2);          
+        }
+
+        this._inc8 = (res * .20).toFixed(0);
+        this._inc8Perc = ( ( ( res * .20) /  169548 ) * 100).toFixed(2);    
+        
+        this._inc7 = (res * .13).toFixed(0);
+        this._inc7Perc = ( ( ( res * .13) / 139502 ) * 100).toFixed(2);  
+        
+        this._inc6 = (res * .02);
+        this._inc6Perc = ( ( ( res * .02) / 181352 ) * 100).toFixed(2);  
+
+      } else if(perc >= 20 && perc < 30) {
+        if((res * .50) >= 79409) {
+          this._inc9 = 79409;   
+          this._inc9Perc = (100).toFixed(2);          
+        } else {
+          this._inc9 = (res * .50).toFixed(0);
+          this._inc9Perc = ( ( ( res * .50) /  79409 ) * 100).toFixed(2);          
+        }
+
+        this._inc8 = (res * .25).toFixed(0);
+        this._inc8Perc = ( ( ( res * .25) /  169548 ) * 100).toFixed(2);    
+        
+        this._inc7 = (res * .17).toFixed(0);
+        this._inc7Perc = ( ( ( res * .17) / 139502 ) * 100).toFixed(2);  
+        
+        this._inc6 = (res * .08).toFixed(0);
+        this._inc6Perc = ( ( ( res * .08) / 181352 ) * 100).toFixed(2);  
+
+      } else if(perc >= 30 && perc < 50) {
+        if((res * .28) >= 79409) {
+          this._inc9 = 79409;   
+          this._inc9Perc = (100).toFixed(2);          
+        } else {
+          this._inc9 = (res * .28).toFixed(0);
+          this._inc9Perc = ( ( ( res * .28) /  79409 ) * 100).toFixed(2);          
+        }
+
+        this._inc8 = (res * .32).toFixed(0);
+        this._inc8Perc = ( ( ( res * .32) /  169548 ) * 100).toFixed(2);    
+        
+        this._inc7 = (res * .25).toFixed(0);
+        this._inc7Perc = ( ( ( res * .25) / 139502 ) * 100).toFixed(2);  
+        
+        this._inc6 = (res * .15).toFixed(0);
+        this._inc6Perc = ( ( ( res * .15) / 181352 ) * 100).toFixed(2);  
+      }
+      
     }
   });
 })();
